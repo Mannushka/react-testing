5ead4c28b8d216c0b510bcee3c3a634e727c9dfd
Mannushka <zaimar1910@gmail.com>
2026-04-04 00:43:18 +0800

    create a helper function render component and refactor the code

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,34 +3,39 @@ import TermsAndConditions from "../../src/components/TermsAndConditions";
 import userEvent from "@testing-library/user-event";
 
 describe("TermsAndConditions", () => {
-  it("should render the terms and conditions text", () => {
+  const renderComponent = () => {
     render(<TermsAndConditions />);
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
+
+    return {
+      heading: screen.getByRole("heading"),
+      checkbox: screen.getByRole("checkbox"),
+      button: screen.getByRole("button", { name: /submit/i }),
+    };
+  };
+
+  it("should render the terms and conditions text", () => {
+    const { heading, checkbox, button } = renderComponent();
+
+    // expect(heading).toBeInTheDocument(); // this line is not necessary because .getByRole will throw an error if the element is not found
     expect(heading).toHaveTextContent("Terms & Conditions");
 
-    const checkbox = screen.getByRole("checkbox");
-    expect(checkbox).toBeInTheDocument();
+    // expect(checkbox).toBeInTheDocument();
     expect(checkbox).not.toBeChecked();
 
-    const button = screen.getByRole("button", { name: /submit/i });
-    expect(button).toBeInTheDocument();
+    // expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
-    expect(button).toHaveTextContent(/submit/i);
   });
 
   it("should enable the submit button when the checkbox is checked", async () => {
     //arrange
-    render(<TermsAndConditions />);
+    const { checkbox, button } = renderComponent();
 
     //act
-    const checkbox = screen.getByRole("checkbox");
     const user = userEvent.setup();
     await user.click(checkbox);
     expect(checkbox).toBeChecked();
 
     //assert
-    const button = screen.getByRole("button");
     expect(button).toBeEnabled();
   });
 });
